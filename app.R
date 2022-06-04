ea90a2e420d3c086bd55cf39a86c50c8dc585b67
Ben Aoki-Sherwood <aokib@carleton.edu>
2022-06-04 15:22:26 -0500

    app skeleton

--- app.R
+++ app.R
@@ -1,14 +1,34 @@
-ui <- fluidPage(
-  varSelectInput(inputId = "my_var", 
-                 label = "Pick a Variable", 
-                 data = select(tweets, charCount, nWords,nRealWords)),
-  tableOutput("stats")
-)
+library(shiny)
+library(tidyverse)
+library(shinyWidgets)
+library(RColorBrewer)
+library(lubridate)
 
+ui <- fluidPage(
+  #make the background interesting
+  setBackgroundColor(
+    color = c("#F7FBFF", "#2171B5"),
+    gradient = "linear",
+    direction = "bottom"),
+  #create title and main panel for entire website page
+  titlePanel("Police Accountability"),
+  mainPanel(
+    #within website page create tabs for the two different plots, each of which will have a sidebar and main layout
+    tabsetPanel(id="main", 
+                tabPanel("Figure 1: ", 
+                         sidebarLayout(sidebarPanel(),
+                                       mainPanel())),
+                tabPanel("Figure 2: ",
+                         sidebarLayout(sidebarPanel(),
+                                       mainPanel())))))
+                                         
 server <- function(input, output){
-  output$stats <- renderTable({
-    tweets %>% 
-      summarize(mean = mean(!!input$my_var), sd = sd(!!input$my_var))
+  output$plot1 <- renderPlot({
+    
+  })
+  
+  output$plot2 <- renderPlot({
+    
   })
 }
 shinyApp(ui = ui, server = server )
