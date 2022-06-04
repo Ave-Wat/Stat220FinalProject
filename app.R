afaaeb315a4972042b080ce2a3a1b774bb282909
wattsa2 <wattsa2@maize.mathcs.carleton.edu>
2022-06-04 16:42:08 -0500

    leaflet work in progress

--- app.R
+++ app.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(shinyWidgets)
 library(RColorBrewer)
 library(lubridate)
+library(leaflet)
 
 ui <- navbarPage(
   "Police Accountability",
@@ -12,8 +13,11 @@ ui <- navbarPage(
            p("This website hopes to show a precursory analysis of the relationships between police brutality, demographics, and police residence.")
   ),
   tabPanel("Maps", 
-          sidebarLayout(sidebarPanel(),
-                        mainPanel())
+          sidebarLayout(
+            sidebarPanel(),
+            mainPanel(
+              leafletOutput("residence_map", width = "100%", height = "100%")
+            ))
   ),
   tabPanel("Scatterplots",
           sidebarLayout(sidebarPanel(),
@@ -34,8 +38,19 @@ ui <- navbarPage(
 )
                                          
 server <- function(input, output){
-  output$plot1 <- renderPlot({
-    
+  output$residence_map <- renderLeaflet({
+    leaflet(joined_cities) %>%
+      addTiles() %>%
+      fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat))
+      
+  })
+  
+  observe({
+    leafletProxy("residence_map", data = joined_cities) %>%
+      clearShapes() %>%
+      addCircles(radius = ~10^mag/10, weight = 1, color = "#777777",
+                 fillColor = ~pal(mag), fillOpacity = 0.7, popup = ~paste(mag)
+      )
   })
   
   output$plot2 <- renderPlot({
