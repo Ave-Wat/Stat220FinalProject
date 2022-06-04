d7cd99d80ece43948eeba189a518889900707219
Ben Aoki-Sherwood <aokib@carleton.edu>
2022-06-04 16:03:54 -0500

    new demographics dataset & joining cities data

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -1,8 +1,24 @@
 library(tidyverse)
 
+#load datasets
 residence <- read_csv('data/police_residence.csv')
 cities <- read_csv('data/cities.csv')
+demographics <- read_delim('data/us-cities-demographics.csv', delim = ';')
+killings <- read_csv('data/police_killings.csv')
+hate_crimes <- read_csv('data/hate_crimes.csv')
 
-residence %>%
-  mutate(city_name = unlist(str_split(city, ','))[1])
+#standardize city variables accross geographical data sets for joining
+residence <- residence %>%
+  mutate(city = str_to_lower(str_extract(residence$city, '[^,]+(?=(,|$))')))
 
+cities <- cities %>%
+  mutate(city = str_to_lower(CITY), lat = LATITUDE, lon = LONGITUDE) %>%
+  select(c('city', 'lat', 'lon'))
+
+demographics <- demographics %>%
+  mutate(city = str_to_lower(City)) %>%
+  select(-c('City'))
+
+joined_cities <- residence %>%
+  left_join(cities) %>%
+  left_join(demographics)
