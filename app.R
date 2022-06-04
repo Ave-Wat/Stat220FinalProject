4ace9359f4412c6d7aa96ff3c1a29b886573dbf0
wattsa2 <wattsa2@maize.mathcs.carleton.edu>
2022-06-04 16:02:24 -0500

    added about page formatting and citations

--- app.R
+++ app.R
@@ -7,8 +7,9 @@ library(lubridate)
 ui <- navbarPage(
   "Police Accountability",
   tabPanel("About", 
-          sidebarLayout(sidebarPanel(),
-                        mainPanel())
+           HTML('<center><img src="download.png" width="400"></center>'),
+           h3(),
+           p("This website hopes to show a precursory analysis of the relationships between police brutality, demographics, and police residence.")
   ),
   tabPanel("Maps", 
           sidebarLayout(sidebarPanel(),
@@ -21,6 +22,9 @@ ui <- navbarPage(
   tabPanel("Likelihood of death",
            sidebarLayout(sidebarPanel(),
                          mainPanel())
+  ),
+  tabPanel("Citations",
+           p("This website hopes to show a precursory analysis of the relationships between police brutality, demographics, and police residence.")
   ),
   #make the background interesting
   setBackgroundColor(
